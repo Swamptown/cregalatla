48a2ae003427192074fa3d483cc1e439c5ae63bb
rasgajag <schwabjackson@gmail.com>
2026-06-03 21:43:34 -0400

    final push for beta 2

---
+++ minecraft/kubejs/client_scripts/renamescrolls.js
@@ -0,0 +1,6 @@
+ClientEvents.lang('en_us', event => {
+    event.renameItem('mymod:closed_air_scroll','Airbending Scroll'),
+    event.renameItem('mymod:closed_water_scroll','Waterbending Scroll'),
+    event.renameItem('mymod:closed_earth_scroll','Earthbending Scroll'),
+    event.renameItem('mymod:closed_fire_scroll','Firebending Scroll')
+})
